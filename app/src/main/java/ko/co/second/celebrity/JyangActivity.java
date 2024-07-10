793ba868074fbd58bf2f12bbbb4d2cdd0c961214
HYOBINKIM <hyobin2631@gmail.com>
2024-07-10 23:04:51 +0900

    Feat: 리스트뷰 정보 설정, 화면 이동

--- app/src/main/java/ko/co/second/celebrity/JyangActivity.java
+++ app/src/main/java/ko/co/second/celebrity/JyangActivity.java
@@ -1,14 +1,17 @@
 package ko.co.second.celebrity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import ko.co.second.R;
 
@@ -17,17 +20,33 @@ public class JyangActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_jyang);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+        setTitle("쯔양");
+
+        // 리스트뷰 정보 설정
+        final String[] mid = {"히어로즈", "24시", "로스트", "가십걸", "스몰빌", "탐정뭉크"};
+        ListView list = findViewById(R.id.ListView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, mid);
+        list.setAdapter(adapter);
+
+        // 화면 이동 설정
+        View mainView = findViewById(R.id.main);
+        ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
+            WindowInsetsCompat systemInsets = insets;
+            v.setPadding(systemInsets.getInsets(WindowInsetsCompat.Type.systemBars()).left,
+                    systemInsets.getInsets(WindowInsetsCompat.Type.systemBars()).top,
+                    systemInsets.getInsets(WindowInsetsCompat.Type.systemBars()).right,
+                    systemInsets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom);
             return insets;
         });
 
-        ActionBar actionBar = getSupportActionBar(); // ActionBar 객체 가져오기
-        if (actionBar != null) {
-            actionBar.hide(); // ActionBar 숨기기
+        // Edge-to-Edge UI
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowInsetsControllerCompat controller = ViewCompat.getWindowInsetsController(mainView);
+        if (controller != null) {
+            controller.show(WindowInsetsCompat.Type.systemBars());
+            controller.setAppearanceLightStatusBars(true);
         }
     }
 }
