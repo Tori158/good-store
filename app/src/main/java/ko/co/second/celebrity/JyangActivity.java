4f6fa1985af1af7faf37f2fec8a4622aa6aeb76c
HYOBINKIM <hyobin2631@gmail.com>
2024-07-13 19:53:06 +0900

    Feat: 리스트에 storename연결하여 표시

--- app/src/main/java/ko/co/second/celebrity/JyangActivity.java
+++ app/src/main/java/ko/co/second/celebrity/JyangActivity.java
@@ -1,19 +1,26 @@
 package ko.co.second.celebrity;
 
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
-
-import androidx.appcompat.app.ActionBar;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import java.util.ArrayList;
+import java.util.List;
 import ko.co.second.R;
+import ko.co.second.fragment.HomeFragment;
+import ko.co.second.map.Store;
+import ko.co.second.map.StoreManager;
 
 public class JyangActivity extends AppCompatActivity {
 
@@ -21,13 +28,25 @@ public class JyangActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jyang);
-        setTitle("쯔양");
+
+        //앱 타이틀 설정
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        toolbarTitle.setText("쯔양");
+
+        // StoreManager 초기화 및 데이터 가져오기
+        StoreManager storeManager = new StoreManager();
+        List<Store> tzuyangStores = storeManager.getStoresByYoutubeName("쯔양");
+
+        // storeName만 추출하여 리스트에 담기
+        List<String> storeNames = new ArrayList<>();
+        for (Store store : tzuyangStores) {
+            storeNames.add(store.getStoreName());
+        }
 
         // 리스트뷰 정보 설정
-        final String[] mid = {"히어로즈", "24시", "로스트", "가십걸", "스몰빌", "탐정뭉크"};
         ListView list = findViewById(R.id.ListView1);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, mid);
+                android.R.layout.simple_list_item_1, storeNames);
         list.setAdapter(adapter);
 
         // 화면 이동 설정
