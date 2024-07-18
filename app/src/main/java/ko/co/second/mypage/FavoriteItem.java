f344f7119ff3e8a21a422bfdb7acffcf7c125a49
HYOBINKIM <hyobin2631@gmail.com>
2024-07-18 17:07:54 +0900

    Feat: 기본 생성자추가

--- app/src/main/java/ko/co/second/mypage/FavoriteItem.java
+++ app/src/main/java/ko/co/second/mypage/FavoriteItem.java
@@ -1,26 +1,35 @@
 package ko.co.second.mypage;
 
-import android.os.Bundle;
+public class FavoriteItem {
+    private String storeName;
+    private String address;
+    private String phoneNumber;
+    private String youtubeLink; // 유튜브 링크 추가
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    public FavoriteItem() {
+        // Firestore에서 데이터를 읽어오려면 빈 생성자가 필요합니다.
+    }
 
-import ko.co.second.R;
+    public FavoriteItem(String storeName, String address, String phoneNumber, String youtubeLink) {
+        this.storeName = storeName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.youtubeLink = youtubeLink;
+    }
 
-public class FavoriteItem extends AppCompatActivity {
+    public String getStoreName() {
+        return storeName;
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_favorite_item);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getYoutubeLink() {
+        return youtubeLink;
     }
 }
