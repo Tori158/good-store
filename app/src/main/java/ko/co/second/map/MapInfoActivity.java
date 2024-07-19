1f76989c26fc101e4f6c518a5a36774187946bb0
HYOBINKIM <hyobin2631@gmail.com>
2024-07-19 13:33:23 +0900

    Feat: WriteReview에 storename전달

--- app/src/main/java/ko/co/second/map/MapInfoActivity.java
+++ app/src/main/java/ko/co/second/map/MapInfoActivity.java
@@ -2,7 +2,7 @@ package ko.co.second.map;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log; // Log 클래스 임포트
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -93,6 +93,7 @@ public class MapInfoActivity extends AppCompatActivity {
         Button makeReviewButton = findViewById(R.id.makeReviewButton);
         makeReviewButton.setOnClickListener(v -> {
             Intent intent = new Intent(MapInfoActivity.this, WriteReview.class);
+            intent.putExtra("storeName", storeName); // storeName을 전달
             startActivity(intent);
         });
 
